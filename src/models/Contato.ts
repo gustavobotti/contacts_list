bc485d60ddca3c837ae05dd2cc5d35dcb01ed513
Gustavo Botti <bottidev@gmail.com>
2024-01-15 14:19:42 -0300

    Update Contato.ts

--- src/models/Contato.ts
+++ src/models/Contato.ts
@@ -2,11 +2,13 @@ class Contato {
   nome: string
   telefone: string
   email: string
+  id: number
 
-  constructor(nome: string, telefone: string, email: string) {
+  constructor(nome: string, telefone: string, email: string, id: number) {
     this.nome = nome
     this.telefone = telefone
     this.email = email
+    this.id = id
   }
 }
 
